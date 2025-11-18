c49d18f7484197d73989fe2f0fbff68109fabbe1
meteaksoyy <meteaksoy531@gmail.com>
2025-11-18 22:26:44 +0100

    Create verra_bot.py

---
+++ verra_bot.py
@@ -0,0 +1,31 @@
+import requests, json, smtplib, os
+
+API_URL = "https://www.verra.nl/en/realtime-listings/consumer"
+CACHE_FILE = "cache.json"
+
+EMAIL = os.environ["BOT_EMAIL"]
+PASSWORD = os.environ["BOT_PASSWORD"]
+TO_EMAIL = os.environ["BOT_TO"]
+
+def fetch_ids():
+  data = requests.get(API_URL).json()
+  return [item["id"] for item in data]
+
+def notify(msg):
+  server = smtplib.SMTP("smtp.gmail.com", 587)
+  server.starttls()
+  server.login(EMAIL, PASSWORD)
+  server.sendmail(EMAIL, TO_EMAIL, msg)
+
+try:
+  old_ids = json.load(open(CACHE_FILE))
+
+except:
+  old_ids = []
+
+new_ids = fetch_ids()
+added = [i for i in new_ids if i not in old_ids]
+
+if added:
+  notify(f"NEW LISTING APPLY RIGHT NOW :) : {added}")
+  json.dump(new_ids, open(CACHE_FILE, "w"))
